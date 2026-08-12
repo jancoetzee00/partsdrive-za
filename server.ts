95b3f81d2f7e269dcbcb9927f8b33d6b8cbcedbf
jancoetzee00 <jancoetzee00@gmail.com>
2026-08-12 15:30:04 +0200

    Update server.ts
    Signed-off-by: jancoetzee00 <jancoetzee00@gmail.com>

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { createServer as createViteServer } from 'vite';
 import { GoogleGenAI, Type } from '@google/genai';
 
 const app = express();
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
 
 // Parse JSON bodies
 app.use(express.json());
